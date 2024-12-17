336d85e6e45889427b9c6b33376c2ef5d02f9d1f
iillya <liuwenbodnss@gmail.com>
2024-12-17 12:57:34 +0800

    2024/12/17 p38

--- Source/UeGAS/Public/Actor/AuraEffectActor.h
+++ Source/UeGAS/Public/Actor/AuraEffectActor.h
@@ -8,6 +8,7 @@
 #include "AuraEffectActor.generated.h"
 
 class USphereComponent;
+class UGameplayEffect;
 
 UCLASS()
 class UEGAS_API AAuraEffectActor : public AActor
@@ -18,21 +19,18 @@ public:
 
 	AAuraEffectActor();
 
-	UFUNCTION()
-	virtual void OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,UPrimitiveComponent* OtherComp,int32 OtherBodyIndex,bool bFromSweep,const FHitResult& SweepResult);
-	UFUNCTION()
-	virtual void EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
 
 protected:
-
 	virtual void BeginPlay() override;
+
+	UFUNCTION(BlueprintCallable)
+	void ApplyEffectToTarget(AActor* TargetActor,TSubclassOf<UGameplayEffect> GameplayEffectClass);
+	UPROPERTY(EditAnywhere,BlueprintReadOnly,Category="AppliedEffects")
+	TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
+
+	
 private:
-	UPROPERTY(VisibleAnywhere)
-	TObjectPtr<USphereComponent> Sphere;
-
-
-	UPROPERTY(VisibleAnywhere)
-	TObjectPtr<UStaticMeshComponent> Mesh;
 
 
 };
