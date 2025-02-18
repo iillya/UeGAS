c775ed92394d842d679103f98fc3d9c3fe2ff2ad
iillya <liuwenbodnss@gmail.com>
2025-02-18 18:38:35 +0800

    2025/2/18 p121

--- Source/UeGAS/Private/Character/AuraCharacterBase.cpp
+++ Source/UeGAS/Private/Character/AuraCharacterBase.cpp
@@ -5,6 +5,7 @@
 #include "AbilitySystemComponent.h"
 #include "AbilitySystem/AuraAbilitySystemComponent.h"
 #include "Components/CapsuleComponent.h"
+#include "UeGAS/UeGAS.h"
 
 // Sets default values
 AAuraCharacterBase::AAuraCharacterBase()
@@ -13,6 +14,9 @@ AAuraCharacterBase::AAuraCharacterBase()
 
 	GetCapsuleComponent()->SetCollisionResponseToChannel(ECC_Camera,ECR_Ignore);
 	GetMesh()->SetCollisionResponseToChannel(ECC_Camera,ECR_Ignore);
+	GetMesh()->SetCollisionResponseToChannel(ECC_Projectile,ECR_Overlap);
+	GetMesh()->SetGenerateOverlapEvents(true);
+	
 	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
 	Weapon->SetupAttachment(GetMesh(),FName("WeaponHandSocket"));
 	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
