ba26dcd6cb18fc8e31f6ae3d5fb378f195c80ba9
iillya <liuwenbodnss@gmail.com>
2025-04-03 18:42:07 +0800

    2025/4/3 P127

---
+++ Source/UeGAS/Public/AbilitySystem/Data/CharacterClassInfo.h
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffect.h"
+#include "Engine/DataAsset.h"
+#include "CharacterClassInfo.generated.h"
+
+UENUM(BlueprintType)
+enum class ECharacterClass : uint8
+{
+	Elementalist,
+	Warrior,
+	Ranger
+	
+};
+
+USTRUCT(BlueprintType)
+struct FCharacterClassDefailtInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly,Category = "Class Defaults")
+	TSubclassOf<UGameplayEffect> PrimaryAttribute;
+};
+/**
+ * 
+ */
+UCLASS()
+class UEGAS_API UCharacterClassInfo : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(EditDefaultsOnly,Category = "Character Class Defaults")
+	TMap<ECharacterClass,FCharacterClassDefailtInfo> CharacterClassInformation;
+	
+	UPROPERTY(EditDefaultsOnly,Category = "Class Defaults")
+	TSubclassOf<UGameplayEffect> SecondaryAttribute;
+
+	UPROPERTY(EditDefaultsOnly,Category = "Class Defaults")
+	TSubclassOf<UGameplayEffect> VitalAttribute;
+
+	FCharacterClassDefailtInfo GetCharacterClassInfo(ECharacterClass CharacterClass);
+};
